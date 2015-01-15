7c9f51384481b6ccd7dfc5670d4182f41f3ef8f0
darshandzend <darshandzend@gmail.com>
2015-01-15 13:19:16 +0530

    initial commit for conc/

---
+++ concurrency_go/conc/conc_test.go
@@ -0,0 +1,23 @@
+package conc
+
+import (
+	"testing"
+)
+
+func TestFold(t *testing.T) {
+	sum := func(a Box, b Box) Box {
+		return a.(int) + b.(int)
+	}
+	numbers := make(chan Box)
+	go func() {
+		for i := 0; i < 10; i++ {
+			numbers <- i
+		}
+		close(numbers)
+	}()
+	totalSum := Fold(numbers, sum)
+
+	if totalSum.(int) != 45 {
+		t.Fail()
+	}
+}
